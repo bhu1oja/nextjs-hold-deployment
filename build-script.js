d019bbe03f5ddd94fd8bf8ef89beb4b5cd252e9c
Ben Ojha <ben@beforeyoubid.com.au>
2024-02-27 17:06:47 +1100

    added node script

--- build-script.js
+++ build-script.js
@@ -1,6 +1,6 @@
 if (process.env.VERCEL_ENV === 'preview') {
     process.exit(1);
-  } else if (process.env.VERCEL_ENV === 'production' && process.env.MY_ENV_VARIABLERR) {
+  } else if (process.env.VERCEL_ENV === 'production' && process.env.MY_ENV_VARIABLERRR) {
     process.exit(1);
   } else {
     process.exit(0);
