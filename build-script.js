4364c1e27d1a69cd8fd96d8bacfb4079099d09ef
Ben Ojha <ben@beforeyoubid.com.au>
2024-02-29 11:29:35 +1100

    added new var fo prod

--- build-script.js
+++ build-script.js
@@ -1,6 +1,6 @@
 if (process.env.VERCEL_ENV === 'preview') {
     process.exit(1);
-  } else if (process.env.VERCEL_ENV === 'production' && process.env.NEW_VAL) {
+  } else if (process.env.VERCEL_ENV === 'production' && process.env.TRIGGER_PROD_BUILD == 'true') {
     process.exit(1);
   } else {
     process.exit(0);
